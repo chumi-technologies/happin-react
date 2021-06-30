616a38b22155f86666ed0f8941cc0037d6020469
IYCI <10443152+IYCI@users.noreply.github.com>
2021-06-30 13:42:10 +0800

    fix build

--- apps/sso/pages/email/index.tsx
+++ apps/sso/pages/email/index.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/Link';
+import Link from 'next/link';
 import React, { useState } from 'react';
 import { firebaseClient } from '../../api/firebaseClient';
 import { FormControl, FormErrorMessage, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
