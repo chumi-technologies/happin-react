e3435bd4b134a0f770a130d48ecf683c957baa15
IYCI <10443152+IYCI@users.noreply.github.com>
2021-07-17 12:15:00 +0800

    fix box office

--- apps/box-office/api/activity.ts
+++ apps/box-office/api/activity.ts
@@ -1,20 +1,20 @@
 import axios from 'axios';
 
 export async function getEvents() {
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZmNlZTBiNTgxMDNlNzAyM2E3N2Y5NjkiLCJyb290VXNlcklEIjoiNWZjZWUwOTQ4MTAzZTcwMjNhNzdmOTY4IiwiaGFwcGluVXNlcklEIjoiNWZjZWUwOTQ1NjIxNTYwMDA5YjJlODU2IiwiaWF0IjoxNjIzMjYzMDMxLCJleHAiOjE2MjU4NTUwMzF9.0FE0f2nmKEhmJPBvb9eWUPaRJwf91RypPC0z3IJJcz8'
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZmJkYmZmOTVlNWZkODMzMGQ4OWQ2ZDUiLCJyb290VXNlcklEIjoiNWZkY2JjNDQ4ODBkNTMwZjI1ZmVmY2IxIiwiaGFwcGluVXNlcklEIjoiNWZjYWZhM2Q4OTc3ZGIwMDA4MGMyMGVmIiwiaWF0IjoxNjI2NDk1MTg2LCJleHAiOjE2MjkwODcxODZ9.qjyfQANCoHOMJPkhKJcJSYXUhW_NQftj6QW4HliZd-Y';
     const res = await axios({
         method: 'GET',
-        url: process.env.CHUMISERVER_HOST + '/activity?pageSize=8&status=all&sortBy=start-time-desc&showStats=true',
+        url: process.env.NEXT_PUBLIC_CHUMI_SERVER_HOST + '/activity?pageSize=8&status=all&sortBy=start-time-desc&showStats=true',
         headers: {'Authorization': 'Bearer '+ token},
     })
     return res;
 }
 
 export async function getEventById(id: string) {
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZmNlZTBiNTgxMDNlNzAyM2E3N2Y5NjkiLCJyb290VXNlcklEIjoiNWZjZWUwOTQ4MTAzZTcwMjNhNzdmOTY4IiwiaGFwcGluVXNlcklEIjoiNWZjZWUwOTQ1NjIxNTYwMDA5YjJlODU2IiwiaWF0IjoxNjIzMjYzMDMxLCJleHAiOjE2MjU4NTUwMzF9.0FE0f2nmKEhmJPBvb9eWUPaRJwf91RypPC0z3IJJcz8'
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZmJkYmZmOTVlNWZkODMzMGQ4OWQ2ZDUiLCJyb290VXNlcklEIjoiNWZkY2JjNDQ4ODBkNTMwZjI1ZmVmY2IxIiwiaGFwcGluVXNlcklEIjoiNWZjYWZhM2Q4OTc3ZGIwMDA4MGMyMGVmIiwiaWF0IjoxNjI2NDk1MTg2LCJleHAiOjE2MjkwODcxODZ9.qjyfQANCoHOMJPkhKJcJSYXUhW_NQftj6QW4HliZd-Y';
     const res = await axios({
         method: 'GET',
-        url: process.env.CHUMISERVER_HOST + '/activity/' + id,
+        url: process.env.NEXT_PUBLIC_CHUMI_SERVER_HOST + '/activity/' + id,
         headers: {'Authorization': 'Bearer '+ token},
     })
     return res;
