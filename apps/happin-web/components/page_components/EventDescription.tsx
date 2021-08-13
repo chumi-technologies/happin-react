eb9b80c497de78379f0df4326b27a2f33bb61181
迈然 <jefn76@gmail.com>
2021-08-13 10:10:24 +0800

    add Description modal

--- apps/happin-web/components/page_components/EventDescription.tsx
+++ apps/happin-web/components/page_components/EventDescription.tsx
@@ -1,6 +1,16 @@
 import Link from 'next/link';
+import { Dialog, Transition } from '@headlessui/react';
+import React, { Fragment, useState } from 'react';
+import { CloseSmall } from '@icon-park/react';
 
 const EventDescription = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const closeModal = () => {
+    setIsOpen(false)
+  }
+  const openModal = () => {
+    setIsOpen(true)
+  }
   return (
     <>
       <div className="black-title text-xl sm:text-2xl font-semibold">Description</div>
@@ -13,7 +23,78 @@ const EventDescription = () => {
           <Link href="#"><a className="mr-2 link-blue">Instagram,</a></Link>
           <Link href="#"><a className="link-blue">Spotify</a></Link>
         </div>
+        <div className="link-rose inline-block cursor-pointer font-medium" onClick={openModal}>More...</div>
       </div>
+      {/*Dialog*/}
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-50 overflow-y-auto"
+          onClose={() => {
+            console.log(111)
+          }}
+        >
+          <div className="min-h-screen px-2 sm:px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="mask-enter"
+              enterFrom="mask-enter-from"
+              enterTo="mask-enter-to"
+              leave="mask-leave"
+              leaveFrom="mask-leave-from"
+              leaveTo="mask-leave-to"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-70" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="dialog-enter"
+              enterFrom="dialog-enter-from"
+              enterTo="dialog-enter-to"
+              leave="dialog-leave"
+              leaveFrom="dialog-leave-from"
+              leaveTo="dialog-leave-to"
+            >
+              <div className="event-details__description-modal">
+                <div className="relative flex items-center mb-6 px-6 pt-6">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-xl font-bold leading-6"
+                  >
+                    Description
+                  </Dialog.Title>
+                  <div className="flex items-center justify-center absolute right-3 w-10 h-10 rounded-full hover:bg-gray-200 hover:text-gray-700 transition cursor-pointer text-gray-500" onClick={closeModal}>
+                    <CloseSmall theme="outline" size="22" fill="currentColor" strokeWidth={3}/>
+                  </div>
+                </div>
+                <div className="flex-1 h-0 overflow-y-auto pb-6">
+                  <div className="px-6">
+                    {/*Description content here*/}
+                    <div className="text-sm sm:text-base">
+                      NYC legend Skyzoo comes to Berlin Under A to perform an exclusive first
+                      look at his new album 'All The Brilliant Things' the night before it
+                      drops!
+                      <div>
+                        <Link href="#"><a className="mr-2 link-blue">Website,</a></Link>
+                        <Link href="#"><a className="mr-2 link-blue">Instagram,</a></Link>
+                        <Link href="#"><a className="link-blue">Spotify</a></Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
     </>
   );
 };
