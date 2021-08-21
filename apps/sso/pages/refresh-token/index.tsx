1037f624a04f2f424b4d2e3f49b4b32b3f211031
Sam <samlaw829@gmail.com>
2021-08-20 20:47:00 -0400

    get the refresh token page origin query param

--- apps/sso/pages/refresh-token/index.tsx
+++ apps/sso/pages/refresh-token/index.tsx
@@ -1,20 +1,23 @@
-import { useAppState } from 'contexts/state';
 import React, { useEffect } from 'react';
 import { auth } from '../../api/firebaseClient';
+import { useRouter } from 'next/dist/client/router'
 
 
 export default function RefreshToken() {
-  const { origin } = useAppState();
+  const router = useRouter()
 
   useEffect(() => {
-    auth.onAuthStateChanged(async user => {
-      if (user) {
-        const token = await user.getIdToken(true);
-        console.log('token refreshed:', token)
-        window.parent.postMessage({ action: 'refresh_token', payload: { token: token } }, origin);
-      }
-    })
-  }, [])
+    if (router.query && Object.keys(router.query).length) {
+      auth.onAuthStateChanged(async user => {
+        if (user) {
+          const { origin } = router.query;
+          const token = await user.getIdToken(true);
+          console.log('token refreshed:', token)
+          window.parent.postMessage({ action: 'refresh_token', payload: { token: token } }, origin as string);
+        }
+      })
+    }
+  }, [router?.query])
   return (
     <></>
   )
