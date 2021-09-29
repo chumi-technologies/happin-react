1c3344ef1977aa1c9446d62c0e2a58a85f1f667f
Sam <samlaw829@gmail.com>
2021-09-29 00:25:18 -0400

    update default seo

--- apps/happin-web/utils/constants.ts
+++ apps/happin-web/utils/constants.ts
@@ -3,11 +3,11 @@
 /**
  * SEO constants
  */
-export const SEO_TITLE = 'Happin Ticketing';
+export const SEO_TITLE = 'Happin | Deep connections with like-minded people through events';
 export const PRODUCTION_URL = 'https://web.happin.app';
 export const SEO_DESCRIPTION =
-  'Create your event for free and start selling tickets online today. CrowdCore is made for event organizers, promoters and venues. Use our end-to-end event management platform to plan, promote and sell-out your live events, conferences and festivals.';
+  'Happin is the easiest way to find like-minded people based on events, and talk over voice, video, and text. You can share or create all kinds of events and stay close with your friends and communities.';
 export const SEO_KEYWORDS =
-  'sell tickets online, event management software, sell concert tickets online, how to sell tickets online, best way to sell tickets online, sell event tickets online, free event management software, how to sell concert tickets online, corporate event management software, sell tickets online free';
+  'Happin, audio event, audio livestream event, free livestream, livestream event, event social media, chat with attendees, chat with fans, event ticketing, free event ticketing';
 export const SEO_OG_IMAGE = '/favicon.png';
 export const SEO_TWITTER_IMAGE = '/favicon.png';
