2443015fcdf3d03a2ae30ac7a12719ddd74ff938
Sam <samlaw829@gmail.com>
2021-10-05 16:14:58 -0400

    add tab set for 'about' and 'agenda'

--- apps/happin-web/components/page_components/EventPageComponents/EventSection.tsx
+++ apps/happin-web/components/page_components/EventPageComponents/EventSection.tsx
@@ -4,10 +4,14 @@ import EventDescription from "./EventDescription";
 import EventAgenda from "./EventAgenda";
 import EventHost from "./EventHost";
 import { EventData } from "lib/model/event";
+import classnames from 'classnames';
+import { useState } from "react";
 
 const EventSection = ({ setIsModalOpen, eventData, groupEvents, setIsRedeemModalOpen }: {
   setIsModalOpen: (arg: boolean) => void, eventData: EventData, groupEvents: any, setIsRedeemModalOpen: (arg: boolean) => void
 }) => {
+  const [tabCur, setTabCur] = useState(0)
+
   return (
     <>
       <EventTitle
@@ -22,24 +26,34 @@ const EventSection = ({ setIsModalOpen, eventData, groupEvents, setIsRedeemModal
         category={eventData?.event?.category}
         categoryType={eventData?.event?.categoryType}
         playbackStart={!!eventData?.event?.ODPBStart}
-        setIsRedeemModalOpen={setIsRedeemModalOpen}/>
-      <div id="about" className="pt-6 sm:pt-10">
-        <EventDescription
-          description={eventData?.event?.content}
-          rawDescription={eventData?.event?.contentPlainText}/>
-        <div className="h-px bg-gray-600 mt-6 sm:mt-10" />
-         {/*<EventLineUp />*/}
-        {/* <div className="h-px bg-gray-600 mt-6 sm:mt-10" /> */}
+        setIsRedeemModalOpen={setIsRedeemModalOpen} />
+      <div className="flex w-full p-1 border border-solid border-gray-600 rounded-full mt-10">
+        <div className={classnames('event-details__tab', { active: tabCur === 0 })} onClick={() => { setTabCur(0) }}>
+          About
+        </div>
+        <div className={classnames('event-details__tab', { active: tabCur === 1 })} onClick={() => { setTabCur(1) }}>
+          Agenda
+        </div>
       </div>
-      <div id="agenda" className="pt-6 sm:pt-10">
-        <EventAgenda eventData={eventData}/>
-        <div className="h-px bg-gray-600 my-6 sm:my-10" />
-        <EventHost
-          hostName={eventData?.event?.creator?.name}
-          hostProfileImageUrl={eventData?.event?.creator?.avatar}
-          hostEmail={eventData?.event?.creator?.email}
-          />
-      </div>
+      {tabCur === 0 &&
+        <div id="about" className="pt-6 sm:pt-10">
+          <EventDescription
+            description={eventData?.event?.content}
+            rawDescription={eventData?.event?.contentPlainText} />
+          <div className="h-px bg-gray-600 my-6 sm:mt-10" />
+        </div>
+      }
+      {tabCur === 1 &&
+        <div id="agenda" className="pt-6 sm:pt-10">
+          <EventAgenda eventData={eventData} />
+          <div className="h-px bg-gray-600 my-6 sm:my-10" />
+        </div>
+      }
+      <EventHost
+        hostName={eventData?.event?.creator?.name}
+        hostProfileImageUrl={eventData?.event?.creator?.avatar}
+        hostEmail={eventData?.event?.creator?.email}
+      />
     </>
   );
 };
