5bc1a64ca5f2858924a37ea969cfc7d01a2894a8
迈然 <jefn76@gmail.com>
2022-01-09 16:30:44 +0800

    add campaign theme

--- apps/happin-web/pages/campaign/index.tsx
+++ apps/happin-web/pages/campaign/index.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { ArrowRight, Help, Lightning, Like, Switch } from "@icon-park/react";
+import { ArrowRight, Help, Lightning, Like, Stopwatch, Switch } from '@icon-park/react';
 import classnames from "classnames";
 import SvgIcon from "@components/SvgIcon";
 import { SearchIcon } from '@chakra-ui/icons';
 import { Avatar } from '@chakra-ui/react';
+import Countdown, { zeroPad } from 'react-countdown';
 const dayList = [
   {
     id: 1,
@@ -31,11 +32,37 @@ const dayList = [
     date: '2.7'
   },
 ];
+const renderer = ({ days, hours, minutes, seconds, completed }: any) => {
+  if (completed) {
+    console.log('finish');
+  } else {
+    return (
+      <div className="ml-2 flex items-center text-white">
+        <div className="campaign__clock">
+          <div>{days}</div>
+        </div>
+        <div className="text-xs font-medium mx-0.5">Days</div>
+        <div className="campaign__clock">
+          {zeroPad(hours)}
+        </div>
+        <div className="text-xs font-medium mx-0.5">Hr</div>
+        <div className="campaign__clock">
+          {zeroPad(minutes)}
+        </div>
+        <div className="text-xs font-medium mx-0.5">M</div>
+        <div className="campaign__clock">
+          {zeroPad(seconds)}
+        </div>
+        <div className="text-xs font-medium mx-0.5">S</div>
+      </div>
+    );
+  }
+};
 const campaign = () => {
   const [tabCur, setTabCur] = useState(2);
   return (
-    <div className="overflow-y-auto">
-      <div className="bg-yellow-500 pt-8">
+    <div className="overflow-y-auto theme-yellow">
+      <div className="campaign__banner pt-8">
         <img className="w-2/3 mx-auto mb-5" src="/images/happy-birthday.png" alt="" />
         <div className="text-center pb-4">
           <div className="black-title font-bold text-xl px-3">
@@ -47,57 +74,129 @@ const campaign = () => {
           </div>
         </div>
       </div>
-      <div className="sticky top-0 flex flex-nowrap overflow-x-auto bg-yellow-500 z-10">
+      <div className="campaign__tabs">
         <div className={classnames('campaign__tab-item black-title', {active: tabCur === 0})} onClick={() => setTabCur(0)}>
           About<br/>Campaign
         </div>
         <div className={classnames('campaign__tab-item black-title', {active: tabCur === 1})} onClick={() => setTabCur(1)}>
-          Voting
+          Actions
         </div>
         <div className={classnames('campaign__tab-item black-title', {active: tabCur === 2})} onClick={() => setTabCur(2)}>
-          Top 10<br/>Ranking
+          Prize
         </div>
         <div className={classnames('campaign__tab-item black-title', {active: tabCur === 3})} onClick={() => setTabCur(3)}>
-          Tab-222
+          Voting
+        </div>
+        <div className={classnames('campaign__tab-item black-title', {active: tabCur === 4})} onClick={() => setTabCur(4)}>
+          Top 10<br/>Ranking
         </div>
       </div>
-      <div className={classnames('px-4 pt-4', {hidden: tabCur !== 0})}>
+      <div className={classnames('pt-4', {hidden: tabCur !== 0})}>
         <div className="mb-5">
-          <h2 className="text-base font-semibold mb-2 text-gray-50">About This Campaign</h2>
-          <img className="my-4 rounded-sm" src="https://freephotos.cc/storage/preview/path/birthday-party-2210x1473.jpg" alt="" />
-          <p className="mb-2 text-gray-200 text-sm">About This Campaign Desciprtion React Native platform-independent tabs. Could be used for bottom tab bars as well as sectioned views</p>
-          <div className="flex text-center mt-5 -ml-1 -mr-1">
-            {
-              dayList.map(item => (
-                <div key={item.id} className="campaign__day-item">
-                  {
-                    item.id < 5 ? <div className="campaign__day" /> : (
-                      <div className="campaign__day">
-                        <div className="campaign__day-last">
-                          <img className="w-3" src="/images/happin-single.svg" alt="" />
+          <div className="px-4">
+            <h2 className="text-base font-semibold mb-2 text-gray-50">About This Campaign</h2>
+          </div>
+          <div className="flex items-center py-1 px-4">
+            <Stopwatch theme="outline" size="18" fill="#fff"/>
+            <span className="text-white uppercase ml-2 flex-1 font-semibold text-tiny">Days Left</span>
+            <Countdown date={new Date(2022, 1, 1)} renderer={renderer} />
+          </div>
+          <div className="px-4">
+            <img className="my-4 rounded-sm" src="https://freephotos.cc/storage/preview/path/birthday-party-2210x1473.jpg" alt="" />
+            <p className="mb-2 text-gray-200 text-sm">About This Campaign Desciprtion React Native platform-independent tabs. Could be used for bottom tab bars as well as sectioned views</p>
+            <div className="flex text-center mt-5 -ml-1 -mr-1">
+              {
+                dayList.map(item => (
+                  <div key={item.id} className="campaign__day-item">
+                    {
+                      item.id < 5 ? <div className="campaign__day" /> : (
+                        <div className="campaign__day">
+                          <div className="campaign__day-last">
+                            <img className="w-3" src="/images/happin-single.svg" alt="" />
+                          </div>
                         </div>
-                      </div>
-                    )
-                  }
-                  <div className="text-center">
-                    <div className="text-gray-50 text-sm font-semibold">{item.text}</div>
-                    <div className="text-gray-400 text-sm font-medium">{item.date}</div>
+                      )
+                    }
+                    <div className="text-center">
+                      <div className="text-gray-50 text-sm font-semibold">{item.text}</div>
+                      <div className="text-gray-400 text-sm font-medium">{item.date}</div>
+                    </div>
                   </div>
-                </div>
-              ))
-            }
+                ))
+              }
+            </div>
           </div>
         </div>
-        <div className="mb-5">
+        <div className="mb-5 px-4">
           <h2 className="text-base font-semibold mb-2 text-gray-50">How to Participate</h2>
           <p className="mb-2 text-gray-200 text-sm">About This Campaign Desciprtion React Native platform-independent tabs. Could be used for bottom tab bars as well as sectioned views</p>
         </div>
-        <div className="mb-5">
+        <div className="mb-5 px-4">
           <h2 className="text-base font-semibold mb-2 text-gray-50">Campaign Reward</h2>
           <p className="mb-2 text-gray-200 text-sm">About This Campaign Desciprtion React Native platform-independent tabs. Could be used for bottom tab bars as well as sectioned views</p>
         </div>
       </div>
-      <div className={classnames('p-4', {hidden: tabCur !== 1})}>
+      <div className={classnames('pt-4', {hidden: tabCur !== 1})}>
+        <div className="mb-5 px-4">
+          <div className="flex items-center pt-3 pb-1 text-white">
+            <div className="flex-1 text-center">
+              <div className="text-sm font-medium text-white text-opacity-80 uppercase">Your Score</div>
+              <div className="black-title font-bold text-2xl mt-0.5">500</div>
+            </div>
+            <div className="w-px h-6 mx-3 bg-white bg-opacity-20" />
+            <div className="flex-1 text-center">
+              <div className="text-sm font-medium text-white text-opacity-80 uppercase">Total Score</div>
+              <div className="black-title font-bold text-2xl mt-0.5">50366</div>
+            </div>
+          </div>
+        </div>
+        <div className="px-4 mb-3">
+          <div className="rounded-lg p-3 bg-white bg-opacity-10">
+            <div className="font-semibold mb-4 text-gray-50">9 Ways to win</div>
+            <div className="space-y-6">
+              <div className="flex items-center">
+                <div className="flex items-center justify-center w-9 h-9 bg-rose-100 rounded-full">
+                  <Like theme="filled" size="18" fill="#fe4365"/>
+                </div>
+                <div className="mx-3 flex-1">
+                  <div className="font-semibold text-sm leading-5 text-gray-50">Visit Gleam on Facebook</div>
+                  <div className="text-xs text-gray-300">Desc on Facebook</div>
+                </div>
+                <button className="reward__btn campaign">
+                  <span className="flex-1">+20</span>
+                  <img src="/images/coins.png" className="w-5" />
+                </button>
+              </div>
+              <div className="flex items-center">
+                <div className="flex items-center justify-center w-9 h-9 bg-rose-100 rounded-full">
+                  <Lightning theme="filled" size="18" fill="#fe4365"/>
+                </div>
+                <div className="mx-3 flex-1">
+                  <div className="font-semibold text-sm leading-5 text-gray-50">Submit a Selfie</div>
+                  <div className="text-xs text-gray-300">Desc Submit a Selfie</div>
+                </div>
+                <button className="reward__btn campaign finished">Gotten</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={classnames('pt-4', {hidden: tabCur !== 2})}>
+        <div className="text-center font-bold text-lg text-white mb-4">Win Your Gifts</div>
+        <div className="px-4 mb-4">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="rounded-md overflow-hidden">
+              <img src="/images/reward-01.jpg" className="w-full" alt="" />
+              <div className="px-3 py-2 text-white font-medium text-center text-sm bg-gray-700">iPhone 13 256G</div>
+            </div>
+            <div className="rounded-md overflow-hidden">
+              <img src="/images/reward-02.jpg" className="w-full" alt="" />
+              <div className="px-3 py-2 text-white font-medium text-center text-sm bg-gray-700">Dyson V12</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={classnames('p-4', {hidden: tabCur !== 3})}>
         <div className="relative flex items-center">
           <label htmlFor="search" className="absolute left-4 leading-none inline-flex transition text-gray-400">
             <SearchIcon w={4} h={4} color="currentColor" />
@@ -167,7 +266,7 @@ const campaign = () => {
           </div>
         </div>
       </div>
-      <div className={classnames({hidden: tabCur !== 2})}>
+      <div className={classnames({hidden: tabCur !== 4})}>
         <div className="campaign__top-three flex pt-9">
           <div className="flex-1 text-center">
             <div className="flex items-center h-24">
@@ -246,9 +345,6 @@ const campaign = () => {
             </div>
           </div>
         </div>
-      </div>
-      <div className={classnames('p-4', {hidden: tabCur !== 3})}>
-        <div className="text-white">Tab-222</div>
       </div>
     </div>
   )
