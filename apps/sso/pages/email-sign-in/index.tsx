e726550855526c58a23e06670933a116d534a9d4
jefn76 <jefn76@gmail.com>
2022-10-13 14:49:06 +0800

    remove icon-park plugin

--- apps/sso/pages/email-sign-in/index.tsx
+++ apps/sso/pages/email-sign-in/index.tsx
@@ -4,7 +4,6 @@ import { firebaseClient } from '../../api/firebaseClient';
 import { getAdminToken } from '../../api/happin-server';
 import { FormControl, FormErrorMessage, Input, InputGroup, InputRightElement } from '@chakra-ui/react';
 import { Formik, Form, Field, FieldProps, FormikHandlers, FormikConfig, FormikValues } from 'formik';
-import { PreviewCloseOne, PreviewOpen } from '@icon-park/react';
 import { ERole, useAppState } from '../../contexts/state';
 import { SubmitButton } from '@components/SubmitButton';
 import { getHappinWebURL, getSaaSDashboardURL } from 'utils/redirect-url';
@@ -114,7 +113,7 @@ export default function EmailSignIn() {
                       />
                       <InputRightElement width="3rem">
                         <div className="leading-none p-1 cursor-pointer" onClick={() => setShowPWD(s => !s)}>
-                          {showPWD ? <PreviewOpen size="18" fill="#9ca3af"/> : <PreviewCloseOne size="18" fill="#9ca3af"/>}
+                          {showPWD ? <svg width="18" height="18" viewBox="0 0 48 48" fill="none"><path d="M24 36C35.0457 36 44 24 44 24C44 24 35.0457 12 24 12C12.9543 12 4 24 4 24C4 24 12.9543 36 24 36Z" fill="none" stroke="#9ca3af" strokeWidth="4" strokeLinejoin="round"/><path d="M24 29C26.7614 29 29 26.7614 29 24C29 21.2386 26.7614 19 24 19C21.2386 19 19 21.2386 19 24C19 26.7614 21.2386 29 24 29Z" fill="none" stroke="#9ca3af" strokeWidth="4" strokeLinejoin="round"/></svg> : <svg width="18" height="18" viewBox="0 0 48 48" fill="none"><path d="M9.85786 18C6.23858 21 4 24 4 24C4 24 12.9543 36 24 36C25.3699 36 26.7076 35.8154 28 35.4921M20.0318 12.5C21.3144 12.1816 22.6414 12 24 12C35.0457 12 44 24 44 24C44 24 41.7614 27 38.1421 30" stroke="#9ca3af" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/><path d="M20.3142 20.6211C19.4981 21.5109 19 22.6972 19 23.9998C19 26.7612 21.2386 28.9998 24 28.9998C25.3627 28.9998 26.5981 28.4546 27.5 27.5705" stroke="#9ca3af" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/><path d="M42 42L6 6" stroke="#9ca3af" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/></svg>}
                         </div>
                       </InputRightElement>
                     </InputGroup>
